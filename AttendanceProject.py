60f12d07530121684a35b149b2a658bd75bd053e
Govind soni <66885515+govind2002soni@users.noreply.github.com>
2022-03-30 19:23:00 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -64,13 +64,17 @@ while True:
         faceDist = face_recognition.face_distance(encodeListKnown,encodeFace)
         #print(faceDist)
         matchIndex = np.argmin(faceDist)
+        #Box
+        y1,x2,y2,x1 = faceLoc
+        y1, x2, y2, x1 =y1*4, x2*4, y2*4, x1*4
+        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
 
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             #print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 =y1*4, x2*4, y2*4, x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            #y1,x2,y2,x1 = faceLoc
+            #y1, x2, y2, x1 =y1*4, x2*4, y2*4, x1*4
+            #cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
             markAttendance(name)
